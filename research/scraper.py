e508401a9b075eaf331262136ded7311b7553d6c
Rahul Kumar <kumarah@gmail.com>
2015-06-07 20:38:49 -0700

    Data cleanup, need to fix indexing issues

--- research/scraper.py
+++ research/scraper.py
@@ -1,19 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
+import random
 
 header_url = "http://www.finviz.com/screener.ashx?v=111&r=1"
 
 data_url = "http://www.finviz.com/screener.ashx?v=111&r="
 
 url_start = 1
-url_end = 7101
+url_end = 7141
 sym_per_page = 20
 
+pages = range(url_start,url_end,20)
 
-r = requests.get(header_url)
+def scrape_page(url):
+    r = requests.get(header_url)
+    soup = BeautifulSoup(r.content)
+    return soup
 
-soup = BeautifulSoup(r.content)
+soup = scrape_page(header_url)
 
 #header = soup.find_all("tr",{"align" :"center"})
 
@@ -30,6 +36,7 @@ total_stocks = int(str(soup.find_all("td",{"class" : "count-text"})[0].contents[
 index = range(0,total_stocks)
 
 
+
 #info_columns.append(soup.find_all("tr",{"align" : "center"})[0].find_all("td",{"style" : "cursor:pointer;"})[0].text)
 info_columns.append(soup.find_all("tr",{"align" : "center"})[0].find_all("td",{"style" : "cursor:pointer;"})[1].text)
 info_columns.append(soup.find_all("tr",{"align" : "center"})[0].find_all("td",{"style" : "cursor:pointer;"})[2].text)
@@ -42,7 +49,7 @@ info_columns.append(soup.find_all("tr",{"align" : "center"})[0].find_all("td",{"
 
 #data_columns.append(soup.find_all("tr",{"align" : "center"})[0].find_all("td",{"style" : "cursor:pointer;"})[1].text)
 data_columns.append(soup.find_all("tr",{"align" : "center"})[0].find_all("td",{"style" : "cursor:pointer;"})[6].text)
-data_columns.append(soup.find_all("tr",{"align" : "center"})[0].find_all("td",{"style" : "cursor:pointer;"})[7].text)
+#data_columns.append(soup.find_all("tr",{"align" : "center"})[0].find_all("td",{"style" : "cursor:pointer;"})[7].text)
 data_columns.append(soup.find_all("tr",{"align" : "center"})[0].find_all("td",{"style" : "cursor:pointer;"})[8].text)
 data_columns.append(soup.find_all("tr",{"align" : "center"})[0].find_all("td",{"style" : "cursor:pointer;"})[9].text)
 data_columns.append(soup.find_all("tr",{"align" : "center"})[0].find_all("td",{"style" : "cursor:pointer;"})[10].text)
@@ -60,28 +67,55 @@ df_data = pd.DataFrame(index = index, columns = data_columns)
 sym_info_count = range(0,100,5)
 sym_data_count = range(0,115,6)
 
-snum = 0
+for page in pages[0:3]:
+    fetch_url = data_url + str(page)
+    print fetch_url
 
-#info_index = []
-#data_index = []
+    #fetch_url = data_url + str(21)
+    soup = scrape_page(fetch_url)
 
-for i in sym_info_count:
-    info_index = int(soup.find_all("td",{"align":"right","class":"body-table-nw"})[snum].contents[0])
-    df_info[info_columns[1]].ix[info_index] = soup.find_all("td",{"align":"left","class":"body-table-nw"})[i].contents[0].contents[0]
-    df_info[info_columns[1]].ix[info_index] = soup.find_all("td",{"align":"left","class":"body-table-nw"})[i+1].contents[0]
-    df_info[info_columns[2]].ix[info_index] = soup.find_all("td",{"align":"left","class":"body-table-nw"})[i+2].contents[0]
-    df_info[info_columns[3]].ix[info_index] = soup.find_all("td",{"align":"left","class":"body-table-nw"})[i+3].contents[0]
-    df_info[info_columns[4]].ix[info_index] = soup.find_all("td",{"align":"left","class":"body-table-nw"})[i+4].contents[0]
-    snum +=6
+    snum = 0
+
+    for i in sym_info_count:
+        try:
+            info_index = int(soup.find_all("td",{"align":"right","class":"body-table-nw"})[snum].contents[0])-1
+            print info_index
+            df_info[info_columns[0]].ix[info_index] = soup.find_all("td",{"align":"left","class":"body-table-nw"})[i].contents[0].contents[0]
+            df_info[info_columns[1]].ix[info_index] = soup.find_all("td",{"align":"left","class":"body-table-nw"})[i+1].contents[0]
+            df_info[info_columns[2]].ix[info_index] = soup.find_all("td",{"align":"left","class":"body-table-nw"})[i+2].contents[0]
+            df_info[info_columns[3]].ix[info_index] = soup.find_all("td",{"align":"left","class":"body-table-nw"})[i+3].contents[0]
+            df_info[info_columns[4]].ix[info_index] = soup.find_all("td",{"align":"left","class":"body-table-nw"})[i+4].contents[0]
+        except:
+            pass
+        snum +=6
 
 
-for j in sym_data_count:
-    data_index = int(soup.find_all("td",{"align":"right","class":"body-table-nw"})[j].contents[0])
-    df_data[data_columns[0]].ix[data_index] = soup.find_all("td",{"align":"right","class":"body-table-nw"})[j+1].contents[0]
-    df_data[data_columns[1]].ix[data_index] = soup.find_all("td",{"align":"right","class":"body-table-nw"})[j+2].contents[0]
-    df_data[data_columns[2]].ix[data_index] = soup.find_all("td",{"align":"right","class":"body-table-nw"})[j+3].contents[0].contents[0]
-    df_data[data_columns[3]].ix[data_index] = soup.find_all("td",{"align":"right","class":"body-table-nw"})[j+4].contents[0].contents[0]
-    df_data[data_columns[4]].ix[data_index] = soup.find_all("td",{"align":"right","class":"body-table-nw"})[j+5].contents[0]
+    for j in sym_data_count:
+        try:
+            data_index = int(soup.find_all("td",{"align":"right","class":"body-table-nw"})[j].contents[0])-1
+            print data_index
+            if str(soup.find_all("td",{"align":"right","class":"body-table-nw"})[j+1].contents[0]).endswith("B"):
+                df_data[data_columns[0]].ix[data_index] = float(str(soup.find_all("td",{"align":"right","class":"body-table-nw"})[j+1].contents[0]).replace('B',''))*1000
+            elif soup.find_all("td",{"align":"right","class":"body-table-nw"})[j+1].contents[0] == '-':
+                df_data[data_columns[0]].ix[data_index] = 0
+            else:
+                df_data[data_columns[0]].ix[data_index] = str(soup.find_all("td",{"align":"right","class":"body-table-nw"})[j+1].contents[0]).replace('M','')
+            #df_data[data_columns[1]].ix[data_index] = soup.find_all("td",{"align":"right","class":"body-table-nw"})[j+2].contents[0]
+            df_data[data_columns[1]].ix[data_index] = soup.find_all("td",{"align":"right","class":"body-table-nw"})[j+3].contents[0].contents[0]
+            df_data[data_columns[2]].ix[data_index] = float(str(soup.find_all("td",{"align":"right","class":"body-table-nw"})[j+4].contents[0].contents[0]).replace('%',''))
+            df_data[data_columns[3]].ix[data_index] = long(str(soup.find_all("td",{"align":"right","class":"body-table-nw"})[j+5].contents[0]).replace(',',''))
+        except:
+            pass
+
+    wait_seconds = random.randint(5,30)
+    time.sleep(wait_seconds)
+    print 'waiting for:' + str(wait_seconds)
+
+
+
+
+
+
 
 df_info.to_csv('df_info.csv')
 df_data.to_csv('df_data.csv')
