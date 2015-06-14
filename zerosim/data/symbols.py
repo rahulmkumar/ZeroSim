9fc7bef91e08ad71c61c5a166f338ccfc40d748d
Rahul Kumar <kumarah@gmail.com>
2015-06-13 21:41:02 -0700

    SymbolDb class test bench completed

--- zerosim/data/symbols.py
+++ zerosim/data/symbols.py
@@ -8,6 +8,9 @@ from sqlalchemy import create_engine
 import sqlite3
 
 class SymbolDb(object):
+
+    SYMBOL_FILES_PATH = '../../symbols/'
+
     def __init__(self):
         self.l_symlist = []
 
@@ -115,13 +118,13 @@ class SymbolDb(object):
             time.sleep(wait_seconds)
             print 'waiting for:' + str(wait_seconds)
 
-        df_info.to_csv('df_info.csv')
-        df_data.to_csv('df_data.csv')
+        df_info.to_csv(self.SYMBOL_FILES_PATH +'df_info.csv')
+        df_data.to_csv(self.SYMBOL_FILES_PATH +'df_data.csv')
 
 
 
 # The code files for this function were taken from: https://www.quandl.com/resources/useful-lists
-    def get_quandl_codes_us(self,file_path):
+    def get_quandl_codes_us(self):
         sp500 = 'https://s3.amazonaws.com/quandl-static-content/Ticker+CSV%27s/Indicies/SP500.csv'
         sp500_file = 'SP500.csv'
 
@@ -159,124 +162,123 @@ class SymbolDb(object):
 
         for key in file_names:
             symfile = urllib2.urlopen(file_names[key][1])
-            output = open(file_path+file_names[key][0],'wb')
+            output = open(self.SYMBOL_FILES_PATH+file_names[key][0],'wb')
             output.write(symfile.read())
             output.close()
 
 
+    def merge_symbol_files(self):
+        df_data = pd.read_csv(self.SYMBOL_FILES_PATH + 'df_data.csv', index_col=0)
+        df_info = pd.read_csv(self.SYMBOL_FILES_PATH + 'df_info.csv', index_col=0)
+        df_sp500 = pd.read_csv(self.SYMBOL_FILES_PATH + 'SP500.csv', index_col=0)
+        df_djia = pd.read_csv(self.SYMBOL_FILES_PATH + 'DJIA.csv', index_col=0)
+        df_nyse = pd.read_csv(self.SYMBOL_FILES_PATH + 'NYSE.csv', index_col=0)
+        df_nasdaq = pd.read_csv(self.SYMBOL_FILES_PATH + 'NASDAQ.csv', index_col=0)
+        df_nyse100 = pd.read_csv(self.SYMBOL_FILES_PATH + 'NYSE100.csv', index_col=0)
+        df_nasdaq100 = pd.read_csv(self.SYMBOL_FILES_PATH + 'NASD100.csv', index_col=0)
+
+
+        # SQlite database connection
+        engine = create_engine('sqlite:///symbols.db')
+
+        #Create finviz table in database
+        df_merged = pd.merge(df_info,df_data, left_index=True, right_index=True)
+        df_finviz = df_merged.set_index('Ticker')
+        df_finviz.to_sql('finviz',engine,if_exists='replace')
+
+        #Create nyse table in database
+        df_nyse['Exchange'] = 'NYSE'
+        df_nyse.to_sql('nyse',engine,if_exists='replace')
+
+        #Create nyse table in database
+        df_nasdaq['Exchange'] = 'NASDAQ'
+        df_nasdaq.to_sql('nasdaq',engine,if_exists='replace')
+
+        #Create nyse table in database
+        df_sp500['Index'] = 'SP500'
+        df_sp500.to_sql('sp500',engine,if_exists='replace')
+
+        #Create nyse table in database
+        df_nyse100['Index'] = 'NYSE100'
+        df_nyse100.to_sql('nyse100',engine,if_exists='replace')
+
+        #Create nyse table in database
+        df_nasdaq100['Index'] = 'NASD100'
+        df_nasdaq100.to_sql('nasdaq100',engine,if_exists='replace')
+
+        con = sqlite3.connect('symbols.db')
+        drop_table_query = """ DROP TABLE final;"""
+        create_table_query = """ CREATE TABLE final(
+                                "Ticker" TEXT,
+                                "Code" TEXT,
+                                "Exchange" TEXT,
+                                "Index" TEXT,
+                                "Company" TEXT,
+                                "Sector" TEXT,
+                                "Industry" TEXT,
+                                "Country" TEXT,
+                                "MarketCap" FLOAT,
+                                "Change" FLOAT,
+                                "Price" FLOAT,
+                                "Volume" FLOAT);"""
+
+        populate_sql_query = """
+            INSERT INTO final
+            (Ticker,Code,Exchange,[Index],Company,Sector,Industry,Country,MarketCap,Change,Price,Volume)
+            SELECT finviz.Ticker,
+            CASE
+            WHEN nyse.Code NOT NULL THEN nyse.Code
+            WHEN nasdaq.Code NOT NULL THEN nasdaq.Code
+            END as "Code",
+            CASE
+            WHEN nyse.Code NOT NULL THEN 'NYSE'
+            WHEN nasdaq.Code NOT NULL THEN 'NASDAQ'
+            END as "Exchange",
+            CASE
+            WHEN sp500.Code NOT NULL THEN 'SP500'
+            WHEN nyse100.Code NOT NULL THEN 'NYSE100'
+            WHEN nasdaq100.Code NOT NULL THEN 'NASDAQ100'
+            END
+            as "Index", finviz.Company, finviz.Sector, finviz.Industry, finviz.Country, finviz.[Market Cap] as "MarketCap", finviz.Change, finviz.Price, finviz.Volume
+            FROM finviz
+            LEFT OUTER JOIN nyse ON finviz.Ticker = nyse.Ticker
+            LEFT OUTER JOIN nasdaq ON finviz.Ticker = nasdaq.Ticker
+            LEFT OUTER JOIN sp500 ON finviz.Ticker = sp500.Ticker
+            LEFT OUTER JOIN nyse100 ON finviz.Ticker = nyse100.Ticker
+            LEFT OUTER JOIN nasdaq100 ON finviz.Ticker = nasdaq100.Ticker;
+            """
+
+        con.execute(drop_table_query)
+        con.commit()
+        con.execute(create_table_query)
+        con.commit()
+        con.execute(populate_sql_query)
+        con.commit()
+        con.close()
+
+        #engine.close()
+
+    def get_symbols(self):
+        engine = create_engine('sqlite:///symbols.db')
+        read_sql_query = """ SELECT Ticker,Code,Exchange,[Index],Company,Sector,Industry,Country,MarketCap,Change,Price,Volume FROM final"""
+        df_final = pd.read_sql(read_sql_query,engine)
+        df_final = df_final.set_index('Ticker')
+        df_final.to_csv(self.SYMBOL_FILES_PATH + 'df_final.csv')
+        #engine.close()
+
 if __name__ == '__main__':
-    #sym = SymbolDb()
-    #sym.refresh_symbol_files('../symbols/')
-    # change total pages to scrape in function above
-    #sym.scrape_finviz_codes_overview(7141,20)
-    df_data = pd.read_csv('/home/rahul/PycharmProjects/ZeroSim/zerosim/data/df_data.csv', index_col=0)
-    df_info = pd.read_csv('/home/rahul/PycharmProjects/ZeroSim/zerosim/data/df_info.csv', index_col=0)
-    df_sp500 = pd.read_csv('/home/rahul/PycharmProjects/ZeroSim/symbols/SP500.csv', index_col=0)
-    df_djia = pd.read_csv('/home/rahul/PycharmProjects/ZeroSim/symbols/DJIA.csv', index_col=0)
-    df_nyse = pd.read_csv('/home/rahul/PycharmProjects/ZeroSim/symbols/NYSE.csv', index_col=0)
-    df_nasdaq = pd.read_csv('/home/rahul/PycharmProjects/ZeroSim/symbols/NASDAQ.csv', index_col=0)
-    df_nyse100 = pd.read_csv('/home/rahul/PycharmProjects/ZeroSim/symbols/NYSE100.csv', index_col=0)
-    df_nasdaq100 = pd.read_csv('/home/rahul/PycharmProjects/ZeroSim/symbols/NASD100.csv', index_col=0)
+    sym = SymbolDb()
 
+    # Refresh symbol files from Quandl link
+    sym.get_quandl_codes_us()
 
-    # SQlite database connection
-    engine = create_engine('sqlite:///symbols.db')
+    #change total pages to scrape in function above
+    sym.scrape_finviz_codes_overview(7141,20)
 
-    #Create finviz table in database
-    df_merged = pd.merge(df_info,df_data, left_index=True, right_index=True)
-    df_finviz = df_merged.set_index('Ticker')
-    df_finviz.to_sql('finviz',engine,if_exists='replace')
+    # Merge all the symbol files from finviz and quandl into SQLite database
+    sym.merge_symbol_files()
 
-    #Create nyse table in database
-    df_nyse['Exchange'] = 'NYSE'
-    df_nyse.to_sql('nyse',engine,if_exists='replace')
-
-    #Create nyse table in database
-    df_nasdaq['Exchange'] = 'NASDAQ'
-    df_nasdaq.to_sql('nasdaq',engine,if_exists='replace')
-
-    #Create nyse table in database
-    df_sp500['Index'] = 'SP500'
-    df_sp500.to_sql('sp500',engine,if_exists='replace')
-
-    #Create nyse table in database
-    df_nyse100['Index'] = 'NYSE100'
-    df_nyse100.to_sql('nyse100',engine,if_exists='replace')
-
-    #Create nyse table in database
-    df_nasdaq100['Index'] = 'NASD100'
-    df_nasdaq100.to_sql('nasdaq100',engine,if_exists='replace')
-
-    con = sqlite3.connect('symbols.db')
-    drop_table_query = """ DROP TABLE final;"""
-    create_table_query = """ CREATE TABLE final(
-                            "Ticker" TEXT,
-                            "Code" TEXT,
-                            "Exchange" TEXT,
-                            "Index" TEXT,
-                            "Company" TEXT,
-                            "Sector" TEXT,
-                            "Industry" TEXT,
-                            "Country" TEXT,
-                            "MarketCap" FLOAT,
-                            "Change" FLOAT,
-                            "Price" FLOAT,
-                            "Volume" FLOAT);"""
-
-    populate_sql_query = """
-        INSERT INTO final
-        (Ticker,Code,Exchange,[Index],Company,Sector,Industry,Country,MarketCap,Change,Price,Volume)
-        SELECT finviz.Ticker,
-        CASE
-        WHEN nyse.Code NOT NULL THEN nyse.Code
-        WHEN nasdaq.Code NOT NULL THEN nasdaq.Code
-        END as "Code",
-        CASE
-        WHEN nyse.Code NOT NULL THEN 'NYSE'
-        WHEN nasdaq.Code NOT NULL THEN 'NASDAQ'
-        END as "Exchange",
-        CASE
-        WHEN sp500.Code NOT NULL THEN 'SP500'
-        WHEN nyse100.Code NOT NULL THEN 'NYSE100'
-        WHEN nasdaq100.Code NOT NULL THEN 'NASDAQ100'
-        END
-        as "Index", finviz.Company, finviz.Sector, finviz.Industry, finviz.Country, finviz.[Market Cap] as "MarketCap", finviz.Change, finviz.Price, finviz.Volume
-        FROM finviz
-        LEFT OUTER JOIN nyse ON finviz.Ticker = nyse.Ticker
-        LEFT OUTER JOIN nasdaq ON finviz.Ticker = nasdaq.Ticker
-        LEFT OUTER JOIN sp500 ON finviz.Ticker = sp500.Ticker
-        LEFT OUTER JOIN nyse100 ON finviz.Ticker = nyse100.Ticker
-        LEFT OUTER JOIN nasdaq100 ON finviz.Ticker = nasdaq100.Ticker;
-        """
-
-    con.execute(drop_table_query)
-    con.commit()
-    con.execute(create_table_query)
-    con.commit()
-    con.execute(populate_sql_query)
-    con.commit()
-    con.close()
-
-    read_sql_query = """ SELECT Ticker,Code,Exchange,[Index],Company,Sector,Industry,Country,MarketCap,Change,Price,Volume FROM final"""
-
-    df_final = pd.read_sql(read_sql_query,engine)
-    df_final = df_final.set_index('Ticker')
-    df_final.to_csv('df_final.csv')
-
-
-    #df_combined = pd.merge(df_finviz, df_nyse, how='outer',left_index=True, right_index=True)
-    #df_combined.to_csv('df_combined_nyse.csv')
-
-    #df_combined1 = pd.merge(df_combined,df_nasdaq,how='outer',left_index=True, right_index=True)
-    #df_combined1.to_csv('df_combined_nasd.csv')
-#    df_nasdaq.sort_index(by='Code',inplace=True)
- #   df_nasdaq.to_csv('df_nasdaq.csv')
-  #  df_combined1 = pd.merge(df_combined, df_nasdaq, left_index=True, right_index=True)
-
-
-   # df_combined1.to_csv('df_combined.csv')
-
-    #print df_nasdaq
-    #df_sp500.to_csv('df_sp500.csv')
+    # Returns the final table from the database
+    sym.get_symbols()
 
 
