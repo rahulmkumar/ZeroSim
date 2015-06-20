abbd747ab5f0446420b7a64595b26511f058e8a0
Rahul Kumar <kumarah@gmail.com>
2015-06-19 23:25:43 -0700

    Created marketdata class and implemented yahoo data source function

---
+++ zerosim/data/marketdata.py
@@ -0,0 +1,30 @@
+import pandas.io.data as web
+import datetime
+
+class MarketData(object):
+
+    def get_yahoo_data(self, l_sym, start_date, end_date, data_type):
+        start_month = int(start_date.split('/')[0])
+        start_day = int(start_date.split('/')[1])
+        start_year = int(start_date.split('/')[2])
+
+        end_month = int(end_date.split('/')[0])
+        end_day = int(end_date.split('/')[1])
+        end_year = int(end_date.split('/')[2])
+
+        start = datetime.datetime(start_year, start_month, start_day)
+        end = datetime.datetime(end_year, end_month, end_day)
+
+        df_prices = web.DataReader(l_sym, 'yahoo', start, end)
+
+        return df_prices[data_type]
+
+
+
+if __name__ == '__main__':
+
+    data = MarketData()
+
+    test_data = data.get_yahoo_data(['AAPL','GOOGL'], '01/01/2015', '05/31/2015', 'High')
+    test_data.to_csv('../../symbols/AAPL_high.csv')
+
