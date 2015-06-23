51b0a46ecb8e29b04a5b0c81279b094392a6e07e
Rahul Kumar <kumarah@gmail.com>
2015-06-23 15:51:06 -0700

    changed get_yahoo_data to return OHCLAC; changed indicators to accept OHLCAC , Updated indicators. Backed up old indicators

--- zerosim/data/marketdata.py
+++ zerosim/data/marketdata.py
@@ -3,7 +3,7 @@ import datetime
 
 class MarketData(object):
 
-    def get_yahoo_data(self, l_sym, start_date, end_date, data_type):
+    def get_yahoo_data_type(self, l_sym, start_date, end_date, data_type):
         start_month = int(start_date.split('/')[0])
         start_day = int(start_date.split('/')[1])
         start_year = int(start_date.split('/')[2])
@@ -19,12 +19,32 @@ class MarketData(object):
 
         return df_prices[data_type]
 
+    def get_yahoo_data(self, l_sym, start_date, end_date):
+        start_month = int(start_date.split('/')[0])
+        start_day = int(start_date.split('/')[1])
+        start_year = int(start_date.split('/')[2])
+
+        end_month = int(end_date.split('/')[0])
+        end_day = int(end_date.split('/')[1])
+        end_year = int(end_date.split('/')[2])
+
+        start = datetime.datetime(start_year, start_month, start_day)
+        end = datetime.datetime(end_year, end_month, end_day)
+
+        df_prices = web.DataReader(l_sym, 'yahoo', start, end)
+        df_prices = df_prices.swapaxes('items', 'minor')
+
+        #print df_prices['AAPL'].ix[:, 'Close']
+        #print df_prices['AAPL'].index
+
+        return df_prices
 
 
 if __name__ == '__main__':
 
     data = MarketData()
 
-    test_data = data.get_yahoo_data(['AAPL','GOOGL'], '01/01/2015', '05/31/2015', 'High')
-    test_data.to_csv('../../symbols/AAPL_high.csv')
+    test_data = data.get_yahoo_data(['AAPL','GOOGL'], '01/01/2015', '05/31/2015')
+    #print test_data['AAPL'].ix['Close', :]
+    #test_data.to_csv('../../symbols/AAPL_high.csv')
 
--- zerosim/indic_test.py
+++ zerosim/indic_test.py
@@ -8,20 +8,27 @@ def main():
     # Return watchlist
     watch_list = data.WatchlistDb()
     watch_list.get_watchlists('data/watchlistdb.db')
-    ibd50 = watch_list.get_watchlist_by_name('data/watchlistdb.db', 'IBD50')
+    #wlist = watch_list.get_watchlist_by_name('data/watchlistdb.db', 'IBD50')
 
     # Get data for watchlist
+    wlist = ['AAPL','GOOGL']
     dat = data.MarketData()
-    test_data = dat.get_yahoo_data(ibd50, '01/01/2015', '05/31/2015', 'Close')
-    #test_data = dat.get_yahoo_data(['AAPL'], '01/01/2015', '05/31/2015', 'Close')
+    #test_data = dat.get_yahoo_data(ibd50, '01/01/2015', '05/31/2015', 'Close')
+    test_data = dat.get_yahoo_data(wlist, '01/01/2015', '05/31/2015')
 
     ind = ta.Indicators()
 
-    df_bb_u, df_bb_m, df_bb_l = ind.talib_BB(test_data, 20, 2, 2)
+    df_bb_u, df_bb_m, df_bb_l = ind.bb(wlist, test_data, 20, 2, 2)
+    df_ema = ind.ema(wlist, test_data, 20)
+    df_ma = ind.ma(wlist, test_data, 20)
+    df_sma = ind.sma(wlist, test_data, 20)
 
     df_bb_u.to_csv('df_bb_upper.csv')
     df_bb_m.to_csv('df_bb_ma.csv')
     df_bb_l.to_csv('df_bb_lower.csv')
+    df_ema.to_csv('df_ema.csv')
+    df_ma.to_csv('df_ma.csv')
+    df_sma.to_csv('df_sma.csv')
 
 
 if __name__ == '__main__':
