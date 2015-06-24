4b0fe8351ce6291472d26aaef93415ed3e96ad9e
Rahul Kumar <kumarah@gmail.com>
2015-06-24 12:07:39 -0700

    Added ATR, Keltner channels, Ichimoku, ttm_squeeze. Added test scenarios

--- zerosim/indic_test.py
+++ zerosim/indic_test.py
@@ -1,40 +1,91 @@
 __author__ = 'rahul'
 import data
 import ta
+import datetime
 
 
 def main():
+
+    current_time = datetime.datetime.now().time()
+    print 'Start time:' + str(current_time)
+
 
     # Return watchlist
     watch_list = data.WatchlistDb()
     watch_list.get_watchlists('data/watchlistdb.db')
     wlist = watch_list.get_watchlist_by_name('data/watchlistdb.db', 'IBD50')
+    #wlist = ['AAPL','GOOGL']
 
     # Get data for watchlist
-    #wlist = ['AAPL','GOOGL']
     dat = data.MarketData()
     #test_data = dat.get_yahoo_data(ibd50, '01/01/2015', '05/31/2015', 'Close')
     test_data = dat.get_yahoo_data(wlist, '01/01/2015', '05/31/2015')
+    #close_data = dat.get_yahoo_data_type(wlist, '01/01/2015', '05/31/2015', 'Close')
 
     ind = ta.Indicators()
 
-    df_bb_u, df_bb_m, df_bb_l = ind.bb(wlist, test_data, 20, 2, 2)
-    df_ema = ind.ema(wlist, test_data, 20)
-    df_ma = ind.ma(wlist, test_data, 20)
-    df_sma = ind.sma(wlist, test_data, 20)
-
-    event = ta.Events()
-
-    df_ma_cross = event.crossabove_df(df_ema, df_sma)
-
-    df_ma_cross.to_csv('df_ma_cross.csv')
-
+    # Bollinger Bands
+    #df_bb_u, df_bb_m, df_bb_l = ind.bb(wlist, test_data, 20, 2, 2)
     #df_bb_u.to_csv('df_bb_upper.csv')
     #df_bb_m.to_csv('df_bb_ma.csv')
     #df_bb_l.to_csv('df_bb_lower.csv')
-    #df_ema.to_csv('df_ema.csv')
+
+    # Fibonacci EMA's
+    df_ema8 = ind.ema(wlist, test_data, 8)
+    df_ema21 = ind.ema(wlist, test_data, 21)
+    df_ema34 = ind.ema(wlist, test_data, 34)
+    df_ema55 = ind.ema(wlist, test_data, 55)
+    df_ema89 = ind.ema(wlist, test_data, 89)
+
+    #df_ema8.to_csv('df_ema8.csv')
+    #df_ema21.to_csv('df_ema21.csv')
+    #df_ema34.to_csv('df_ema34.csv')
+    #df_ema55.to_csv('df_ema55.csv')
+    #df_ema89.to_csv('df_ema89.csv')
+
+    # Test EMA's
+    #df_ema_old = ind.ema_old(close_data, 21)
+    #df_ema_old.to_csv('df_ema_old.csv')
+
+    #df_ma = ind.ma(wlist, test_data, 20)
     #df_ma.to_csv('df_ma.csv')
+
+    #df_sma = ind.sma(wlist, test_data, 20)
     #df_sma.to_csv('df_sma.csv')
+
+
+    # Keltner Channels
+    #df_kelt_u, df_kelt_m, df_kelt_l = ind.keltner(wlist, test_data, 20, 20, 2)
+    #df_kelt_u.to_csv('df_kelt_u.csv')
+    #df_kelt_m.to_csv('df_kelt_m.csv')
+    #df_kelt_l.to_csv('df_kelt_l.csv')
+
+    # ATR
+    #df_atr = ind.atr(wlist, test_data, 14)
+    #df_atr.to_csv('df_atr.csv')
+
+    # TTM Squeeze Test
+    df_bb_ma, df_bb_u, df_bb_l, df_kch_m, df_kch_u, df_kch_l = ind.ttm_squeeze(wlist, test_data, 21, 2, 21, 21, 1.5 )
+    #df_bb_ma.to_csv('df_bb_ma.csv')
+    #df_bb_u.to_csv('df_bb_u.csv')
+    #df_bb_l.to_csv('df_bb_l.csv')
+    #df_kch_m.to_csv('df_kch_m.csv')
+    #df_kch_u.to_csv('df_kch_u.csv')
+    #df_kch_l.to_csv('df_kch_l.csv')
+
+    # Ichimoku Test
+    df_ichi_tenkan, df_ichi_kijun = ind.ichimoku(wlist, test_data)
+    #df_ichi_tenkan.to_csv('df_ichi_tenkan.csv')
+    #df_ichi_kijun.to_csv('df_ichi_kijun.csv')
+
+    # Event Scanner
+    #event = ta.Events()
+    #df_ma_cross = event.crossabove_df(df_ema, df_sma)
+    #df_ma_cross.to_csv('df_ma_cross.csv')
+    #df_ema.to_csv('df_ema.csv')
+
+    current_time = datetime.datetime.now().time()
+    print 'End time:' + str(current_time)
 
 
 if __name__ == '__main__':
--- zerosim/ta/indicators.py
+++ zerosim/ta/indicators.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import copy
 import talib
+
 
 class Indicators(object):
     """
@@ -17,7 +17,9 @@ class Indicators(object):
         df_bb_l = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
 
         for sym in l_sym:
-            df_bb_u[sym], df_bb_m[sym], df_bb_l[sym] = talib.BBANDS(np.asarray(df_price[sym].ix[:, 'Close']), time_period, st_dev_u, st_dev_l)
+            df_bb_u[sym], df_bb_m[sym], df_bb_l[sym] = talib.BBANDS(np.asarray(df_price[sym].ix[:, 'Close']),
+                                                                    timeperiod=time_period, nbdevup=st_dev_u,
+                                                                    nbdevdn=st_dev_l)
         return df_bb_u, df_bb_m, df_bb_l
 
     def ema(self, l_sym, df_price, time_period):
@@ -44,70 +46,46 @@ class Indicators(object):
 
         return df_sma
 
-    #e = ema length
-    #a = atr length
-    #m = atr multiplier
+    def atr(self, l_sym, df_price, time_period):
+        df_atr = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
 
-    def keltner(self, df_close, df_high, df_low, e, a, m):
-        sym_list = df_close.columns
-        timestamps = df_close.index
+        for sym in l_sym:
+            df_atr[sym] = talib.ATR(high=np.asarray(df_price[sym].ix[:, 'High']), low=np.asarray(df_price[sym].ix[:, 'Low']),
+                                    close=np.asarray(df_price[sym].ix[:, 'Close']), timeperiod=time_period)
+        return df_atr
 
-        df_kch_m = self.ema(df_close,e)
-        df_atr = self.atr(df_close,df_high,df_low,a,m)
+    def keltner(self, l_sym, df_price, ema_period, atr_period, multiplier):
 
-        df_kch_u = copy.deepcopy(df_close)
-        df_kch_u = df_kch_u * np.NAN
+        df_kch_u = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
+        df_kch_l = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
 
-        df_kch_l = copy.deepcopy(df_close)
-        df_kch_l = df_kch_l * np.NAN
+        df_kch_m = self.ema(l_sym, df_price, time_period=ema_period)
+        df_atr = self.atr(l_sym, df_price, time_period=atr_period)
 
-        for sym in sym_list:
-            for t_stamp in range(0,len(timestamps)):
-                df_kch_u[sym].ix[t_stamp] = df_kch_m[sym].ix[t_stamp] + df_atr[sym].ix[t_stamp]
-                df_kch_l[sym].ix[t_stamp] = df_kch_m[sym].ix[t_stamp] - df_atr[sym].ix[t_stamp]
+        for sym in l_sym:
+            df_kch_u[sym] = df_kch_m[sym] + (multiplier * df_atr[sym])
+            df_kch_l[sym] = df_kch_m[sym] - (multiplier * df_atr[sym])
 
-        return df_kch_m, df_kch_u, df_kch_l
+        return df_kch_u, df_kch_m, df_kch_l
 
-    def ichimoku(self, df_close, df_high, df_low):
-        '''
-        df_ichimoku_tenkan_u, df_ichimoku_tenkan_l = indicators.channel(df_close,9)
-        df_ichimoku_kijun_u, df_ichimoku_kijun_l = indicators.channel(df_close,26)
-        df_senkou_a
-        df_senkou_b_u, df_senkou_b_l = indicators.channel(df_close,52)
+    def ichimoku(self, l_sym, df_price):
 
-        sym_list = df_close.columns
-        '''
-        timestamps = df_close.index
-        sym_list = df_close.columns
+        df_ichimoku_tenkan_u = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
+        df_ichimoku_tenkan_l = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
+        df_ichimoku_kijun_u = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
+        df_ichimoku_kijun_l = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
+        df_ichimoku_kijun = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
+        df_ichimoku_tenkan = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
 
-        df_ichimoku_tenkan_u = copy.deepcopy(df_close)
-        df_ichimoku_tenkan_u = df_ichimoku_tenkan_u * np.NAN
+        for sym in l_sym:
+            df_ichimoku_tenkan_u[sym] = pd.rolling_max(df_price[sym].ix[:,'High'], min_periods=1, window=9)
+            df_ichimoku_tenkan_l[sym] = pd.rolling_min(df_price[sym].ix[:,'Low'], min_periods=1, window=9)
 
-        df_ichimoku_tenkan_l = copy.deepcopy(df_close)
-        df_ichimoku_tenkan_l = df_ichimoku_tenkan_l * np.NAN
+            df_ichimoku_kijun_u[sym] = pd.rolling_max(df_price[sym].ix[:,'High'], min_periods=1, window=26)
+            df_ichimoku_kijun_l[sym] = pd.rolling_min(df_price[sym].ix[:,'Low'], min_periods=1, window=26)
 
-        df_ichimoku_kijun_u = copy.deepcopy(df_close)
-        df_ichimoku_kijun_u = df_ichimoku_kijun_u * np.NAN
-
-        df_ichimoku_kijun_l = copy.deepcopy(df_close)
-        df_ichimoku_kijun_l = df_ichimoku_kijun_l * np.NAN
-
-        df_ichimoku_kijun = copy.deepcopy(df_close)
-        df_ichimoku_kijun = df_ichimoku_kijun * np.NAN
-
-        df_ichimoku_tenkan = copy.deepcopy(df_close)
-        df_ichimoku_tenkan = df_ichimoku_tenkan * np.NAN
-
-        for sym in sym_list:
-            df_ichimoku_tenkan_u[sym] = pd.rolling_max(df_high[sym],min_periods=1,window=9)
-            df_ichimoku_tenkan_l[sym] = pd.rolling_min(df_low[sym],min_periods=1,window=9)
-
-            df_ichimoku_kijun_u[sym] = pd.rolling_max(df_high[sym],min_periods=1,window=26)
-            df_ichimoku_kijun_l[sym] = pd.rolling_min(df_low[sym],min_periods=1,window=26)
-
-            for t_stamp in timestamps:
-                df_ichimoku_tenkan[sym].ix[t_stamp] = (df_ichimoku_tenkan_u[sym].ix[t_stamp] + df_ichimoku_tenkan_l[sym].ix[t_stamp])/2
-                df_ichimoku_kijun[sym].ix[t_stamp] = (df_ichimoku_kijun_u[sym].ix[t_stamp] + df_ichimoku_kijun_l[sym].ix[t_stamp])/2
+            df_ichimoku_tenkan[sym] = (df_ichimoku_tenkan_u[sym] + df_ichimoku_tenkan_l[sym])/2
+            df_ichimoku_kijun[sym] = (df_ichimoku_kijun_u[sym] + df_ichimoku_kijun_l[sym])/2
 
         return df_ichimoku_tenkan, df_ichimoku_kijun
 
@@ -116,40 +94,25 @@ class Indicators(object):
     Bollinger Bands: 20,2
     Keltner Channels: 20,1.5
     '''
-    def TTM_squeeze(self, df_price, df_high, df_low, N, K, e, a, m):
-        sym_list = df_price.columns
-        timestamps = df_price.index
+    def ttm_squeeze(self, l_sym, df_price, bb_ma, stdev_multiplier, ema_period, atr_period, atr_multiplier):
 
-        df_bb_ma = copy.deepcopy(df_price)
-        df_bb_ma = df_bb_ma * np.NAN
+        df_bb_ma = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
+        df_bb_u = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
+        df_bb_l = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
+        df_kch_u = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
+        df_kch_l = pd.DataFrame(columns=l_sym, index=df_price[l_sym[0]].index)
 
-        df_bb_u = copy.deepcopy(df_bb_ma)
-        #df_bb_u = df_bb_u * np.NAN
+        df_kch_m = self.ema(l_sym, df_price, ema_period)
+        df_atr = self.atr(l_sym, df_price, atr_period)
 
-        df_bb_l = copy.deepcopy(df_bb_ma)
-        #df_bb_l = df_bb_l * np.NAN
+        for sym in l_sym:
+            df_bb_ma[sym] = pd.rolling_mean(df_price[sym].ix[:, 'Close'], window=bb_ma)
+            df_bb_u[sym] = pd.rolling_std(df_price[sym].ix[:, 'Close'], window=bb_ma)
+            df_bb_l[sym] = pd.rolling_std(df_price[sym].ix[:, 'Close'], window=bb_ma)
+            df_bb_u[sym] = df_bb_ma[sym] + (stdev_multiplier * df_bb_u[sym])
+            df_bb_l[sym] = df_bb_ma[sym] - (stdev_multiplier * df_bb_l[sym])
 
-        df_kch_u = copy.deepcopy(df_price)
-        df_kch_u = df_kch_u * np.NAN
-
-        df_kch_l = copy.deepcopy(df_price)
-        df_kch_l = df_kch_l * np.NAN
-
-        df_kch_m = self.ema(df_price,e)
-        df_atr = self.atr(df_price,df_high,df_low,a,m)
-
-        for sym in sym_list:
-            df_bb_ma[sym] = pd.rolling_mean(df_price[sym],window=N)
-            df_bb_u[sym] = pd.rolling_std(df_price[sym],window=N)
-            df_bb_l[sym] = pd.rolling_std(df_price[sym],window=N)
-            df_bb_u[sym] = df_bb_ma[sym] + (K*df_bb_u[sym])
-            df_bb_l[sym] = df_bb_ma[sym] - (K*df_bb_l[sym])
-            for t_stamp in range(0,len(timestamps)):
-                df_kch_u[sym].ix[t_stamp] = df_kch_m[sym].ix[t_stamp] + df_atr[sym].ix[t_stamp]
-                df_kch_l[sym].ix[t_stamp] = df_kch_m[sym].ix[t_stamp] - df_atr[sym].ix[t_stamp]
+            df_kch_u[sym] = df_kch_m[sym] + (atr_multiplier * df_atr[sym])
+            df_kch_l[sym] = df_kch_m[sym] - (atr_multiplier * df_atr[sym])
 
         return df_bb_ma, df_bb_u, df_bb_l, df_kch_m, df_kch_u, df_kch_l
-
-
-
-
